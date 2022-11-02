439de9ef20d5d4333ec0ab097e62e1f2817bb61b
KashElcock <116089696+KashElcock@users.noreply.github.com>
2022-11-02 14:35:19 -0400

    All tests for original code passed

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,32 +4,31 @@ package com.zipcodewilmington.person;
  * Created by leon on 2/12/18.
  */
 public class Person {
-    private String name;
-    private int age;
-
-    public Person() {
-    }
-
-    public Person(int age) {
-    }
-
+    private String name ="";
+    private int age = Integer.MAX_VALUE;;
+    private int weight = 0;
+    private double shoeSize = 0.0;
+    private double height = 0.0;
+    private String eyeColor = "";
+    private String bloodType = "";
+    private Boolean isAlive = Boolean.TRUE;
+    public Person() {}
+    public Person(int age) {setAge(age);}
     public Person(String name) {
+        setName(name);
     }
-
     public Person(String name, int age) {
+       setName(name);
+       setAge(age);
     }
-
     public void setName(String name) {
+        this.name = name;
     }
-
-    public void setAge(int age) {
-    }
-
-    public String getName() {
-        return null;
-    }
-
+    public void setAge(int age) {this.age = age;}
+    public void setWeight() {this.weight = weight;}
+    public String getName() { return name;}
     public Integer getAge() {
-        return null;
+        return age;
     }
+
 }
